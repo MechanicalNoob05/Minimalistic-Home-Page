ea9aa9567c6f840999b9ebd01e13da104d447072
Mechanical Noob <tejasmayekar08@gmail.com>
2022-11-05 12:09:01 +0530

    completion with quote and time displayed

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -1,11 +1,23 @@
-import React from 'react'
 import './quote.css'
+import React, { useState, useEffect } from 'react'
 
 function Quote() {
+  const [quote , setquote] = useState({
+    anime:null,
+    character:null,
+    quote:null
+  })
+  const fetchquote = async () =>{
+    return await fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json())
+  }
+  useEffect(async()=>{
+    setquote(await fetchquote());
+  },[]);
   return (
     <div className='quote_container'>
-        <p className='quote'>Work is workship, abcde my nae</p>
-        <p className='quote_author'>- tate p mare</p>
+        <p className='quote'>{quote.quote}</p>
+        <p className='quote_author'>{quote.character}</p>
     </div>
   )
 }
--- src/components/time/Clock.js
+++ src/components/time/Clock.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Quote from '../quote/Quote'
 import './clock.css'
+import Moment from 'moment'
 
 function Clock() {
+  const options = {
+    timeZone:"Asia/Kolkata",
+    hour12 : true,
+    hour:  "numeric",
+    minute: "numeric",seconds:"numeric"
+ }
+  let time = new Date().toLocaleTimeString("en-US",options);
+  const [currentTime, setCurrentTime] = useState(time);
+  const updateTime = () => {
+    let time = new Date().toLocaleTimeString("en-US",options);
+    setCurrentTime(time);
+  }
+  setInterval(updateTime,1000);
+
+  let date = Moment().format('DD-MM-YYYY');
+
   return (
     <div className='clock_container'>
-        <h1><span className='hour'>00</span>:<span className='min'>00</span> <span className='am/pm'>AM</span></h1>
+        <h1>{currentTime}</h1>
+        <h2>{date}</h2>
         <Quote></Quote>
     </div>
   )
